57cc7e0155f573059f2a76ce6afb5ae53627a5fd
Dagmawi Solomon <dagisolo1023@gmail.com>
2025-11-27 01:24:32 +0300

    profile change

--- Portfolio Home Page/script.js
+++
@@ -1,7 +0,0 @@
-var typed = new Typed(".multiple-text", {
-    strings: ['Frontend Developer', 'Backend Developer', 'Comunication Engineer'],
-    typeSpeed: 100,
-    backSpeed: 25,
-    backDelay: 1000,
-    loop: true
-})
